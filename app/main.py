ff822c3409c9301eb0ff5205c8b5eea87f448cf1
amithen <35074878+amithen@users.noreply.github.com>
2026-05-18 01:53:17 +0530

    SQL connection and SQLAlchemy setup done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.db.database import engine
+
 
 app = FastAPI()
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def home():
+    return {"message": "AI Interview Assistant API Running"}
+
+@app.get("/health")
+async def health_check():
+    with engine.connect() as connection:
+        connection.execute(text("SELECT 1"))
+    return {"status": "healthy"}
 
